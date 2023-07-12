ca9c89fbaa8d4abafdd3bc2d92bede777fbad5a0
Jason Fortit <json.commits@gmail.com>
2023-07-12 12:04:32 +0800

    Added additional functionality for serialNumbered items.

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,20 @@ sheets.authorize().catch(error => console.log(error));
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-    const item = req.query['name'];
+    let item = req.query['name'];
+    let itemList
+    let quantityValue = ``;
+    if(item){
+        itemList = 'GenericItemList'
+    }
+    else{
+        itemList = 'SerialItemList'
+        item = req.query['serialNumber'];
+        quantityValue = '1';
+    }
 
-    let itemData = await sheets.getRangeData('ItemList!A:D');
+
+    let itemData = await sheets.getRangeData(`${itemList}!B:E`);
     let itemValues = itemData.data.values;
     let itemName = itemValues.map(x => x[0]);
     let request = element => element === item;
@@ -26,18 +37,20 @@ router.get('/', async function (req, res, next) {
             itemAvailableQuantity: `${itemAvailableQuantity}`,
             itemBorrowedQuantity: `${itemBorrowedQuantity}`,
             foundItemIndex: `${foundItemIndex}`,
+            itemList: `${itemList}`,
+            quantityValue: `${quantityValue}`
         });
 });
 
 router.post('/borrow', async function (req, res, next) {
     const item = req.body.item;
+    const itemList = req.body.itemList;
     const name = req.body.name;
     const quantity = Number(req.body.quantity);
     const foundItemIndex = Number(req.body.foundItemIndex) + 1;
     const itemBorrowedQuantity = Number(req.body.itemBorrowedQuantity);
 
-    console.log(`ItemList!C${foundItemIndex}`)
-    await sheets.setRangeData(`ItemList!C${foundItemIndex}`, [itemBorrowedQuantity + quantity]);
+    await sheets.setRangeData(`${itemList}!D${foundItemIndex}`, [itemBorrowedQuantity + quantity]);
     await sheets.appendRangeData(`Logs!A1:C1`, ['=lambda(x,x)(now())', name, item, 'BORROW', quantity]);
 
     res.render('submit', {message: `Borrowed ${item} x ${quantity}`})
@@ -45,13 +58,13 @@ router.post('/borrow', async function (req, res, next) {
 
 router.post('/return', async function (req, res, next) {
     const item = req.body.item;
+    const itemList = req.body.itemList;
     const name = req.body.name;
     const quantity = Number(req.body.quantity);
     const foundItemIndex = Number(req.body.foundItemIndex) + 1;
     const itemBorrowedQuantity = Number(req.body.itemBorrowedQuantity);
 
-    console.log(`ItemList!C${foundItemIndex}`);
-    await sheets.setRangeData(`ItemList!C${foundItemIndex}`, [itemBorrowedQuantity - quantity]);
+    await sheets.setRangeData(`${itemList}!D${foundItemIndex}`, [itemBorrowedQuantity - quantity]);
     await sheets.appendRangeData(`Logs!A1:C1`, ['=lambda(x,x)(now())', name, item, 'RETURN', quantity]);
 
     res.render('submit', {message: `Returned ${item} x ${quantity}`});
